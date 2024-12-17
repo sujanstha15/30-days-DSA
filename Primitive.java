c170e8dc37eabc487f734e162ab37398aad2ad9f
sujanstha15 <raju40rr@gmail.com>
2024-12-16 18:40:22 -0600

    Question and Notes are added in multiilne comment

--- Primitive.java
+++ Primitive.java
@@ -14,5 +14,6 @@ public class Primitive {
  A primtive is the smallest valid box that cannot be broken into smaller independent parts.
  - A counter is required to track the opening and closing of the bracket. 
 
-
+First split into two parts
+then remove the outermost parentheses and add the split parts
  */
