3ca04369d097c55ab8dc90985df0796491fcf574
sujanstha15 <raju40rr@gmail.com>
2024-12-16 18:00:18 -0600

    Primitive.java file is created

---
+++ Primitive.java
@@ -0,0 +1,18 @@
+public class Primitive {
+
+    //method to remove the outer parenthesees
+    public String removeOuterParentheses(String s){
+        
+        return s;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
+/* Notes
+ A primtive is the smallest valid box that cannot be broken into smaller independent parts.
+ - A counter is required to track the opening and closing of the bracket. 
+
+
+ */
