baafbff5bdf0909b641f3b9a813210892c8e8495
sujanstha15 <raju40rr@gmail.com>
2024-12-17 23:15:26 -0600

    A class to count the total number of nodes in a linked list.

---
+++ LL2.java
@@ -0,0 +1,27 @@
+public class LL2 {
+    /*
+     
+    My initial code to find the number of linked list in a linked list
+
+class Solution {
+    // Function to count nodes of a linked list.
+    public int getCount(Node head) {
+        // code here
+        int count=0;
+        Node temp = head;
+        if(head==null){
+            return 0;
+        }
+        
+        while(head!=null){
+            
+            if(temp.next!=null){
+                temp = temp.next;
+                count++;
+            }
+        }
+        return count;
+    }
+}
+     */
+}
