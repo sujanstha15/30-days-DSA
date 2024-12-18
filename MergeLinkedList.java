df0918208981a81d87fd3e573f380dce60ea8480
sujanstha15 <raju40rr@gmail.com>
2024-12-18 17:37:56 -0600

    A class where two linked list will be merged

---
+++ MergeLinkedList.java
@@ -0,0 +1,19 @@
+public class MergeLinkedList {
+    //definition of singly-lined list
+     class ListNode {
+            int val;
+           ListNode next;
+             ListNode() {}
+             ListNode(int val) { this.val = val; }
+             ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        }
+
+        //
+        class Solution{
+            public ListNode mergeTwoLists(ListNode list1, ListNode 2){
+                
+            }
+        }
+
+
+}
