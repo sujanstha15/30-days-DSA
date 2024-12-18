1bc334ddf0bfbafc52dc297b575f67361ee20f4d
sujanstha15 <raju40rr@gmail.com>
2024-12-17 23:07:04 -0600

    Creating a program to add data at the end of linked list

--- LinkedList.java
+++ LinkedList.java
@@ -1,5 +1,36 @@
+
 public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Day 2");
+    class Node {
+        int data;
+        Node next;
+    
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    //inserting node at the end of linked list
+    
+    //Function to insert a node at the end of the linked list
+    Node insertAtEnd(Node head, int x){
+        //create a new node with the given value
+        Node newNode = new Node(x);
+
+        //edge case- if the linked list is empty, the new node becomes the head
+        if(head==null){
+            return newNode;
+        }
+
+        //Traverse to the last node
+        Node current = head;
+        while(current.next!=null){
+            current= current.next;
+        }
+        //point the last node's next to the new node
+        current.next = newNode;
+
+        //return head;
+        return head;
     }
 }
