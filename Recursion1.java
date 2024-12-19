6875deecb9cfe9f6924a42e83230c319b96860b9
sujanstha15 <raju40rr@gmail.com>
2024-12-19 09:22:52 -0600

    A file for recursion technique is create

---
+++ Recursion1.java
@@ -0,0 +1,17 @@
+public class Recursion1 {
+    //recursive method to calculate the sum
+    public static int calcSum(int n){
+        //we always have a base case in recursion, it should be mandatory because our program terminated only when the base case is true
+        //base case for this program
+        if(n==1){
+            return 1;
+        }
+
+        //now this is the recursive call
+        return n+ calcSum(n-1);
+    }
+
+    //main method
+    public static void main(String[] args) {
+        System.err.println(calcSum(3));
+    }}
